af7ddc9c3a2f6e7bd753504cbd7df3093fd4499b
Grom Maksym <MailMaximGrom@gmail.com>
2018-11-11 13:03:49 +0200

    Created the base module structure

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Grom Maksym <MailMaximGrom@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Grom_SyncStaticContent',
+    __DIR__
+);
